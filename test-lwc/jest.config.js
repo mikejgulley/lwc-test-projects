a8c95f8e2d0c238cee4c37fd266e0619c842ed0e
Michael Gulley <mgulley.mograph@gmail.com>
2022-02-11 14:02:46 -0500

    Add remaining code for testing lwcs module

--- test-lwc/jest.config.js
+++ test-lwc/jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
     moduleNameMapper: {
+        '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
+        '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
         '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
     }
 };
